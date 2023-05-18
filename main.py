355d71150b29e2994c05ba70d66fe4c2c922f507
ah19017 <ah19017@melville-high.school.nz>
2023-05-18 15:09:50 +1200

    help started

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 from tkinter import *
 from tkinter import ttk
+history = {}
 
 
 def c_convert_func():  # converts the input to a fahrenheit output
-    temp = fahrenheit_input.get()
+    temp_f = fahrenheit_input.get()
     try:
-        temp = float(temp)
-        temp = (temp - 32) * (5/9)
-        temp = round(temp, 2)
-        if temp < -273.15:  # absolute zero in C
-            temp = str(temp) + "°C"
-            c_out.set("Invalid input, temperature is\nbelow absolute zero.\n({} < 273.15°C)".format(temp))
-            return
-        temp = str(temp) + "°C"
-        c_out.set(temp)
+        temp_f = float(temp_f)
+        temp_c = (temp_f - 32) * (5/9)
+        temp_c = round(temp_c, 2)
+        if temp_c < -273.15:  # absolute zero in C
+            temp_c = str(temp_c) + "°C"
+            c_out.set("Invalid input, temperature is\nbelow absolute zero.\n({} < 273.15°C)".format(temp_c))
+            return  # ends the subroutine
+        temp_c = str(temp_c) + "°C"
+        c_out.set(temp_c)
+        temp_f = str(temp_f) + "°F"
+        history[temp_f] = temp_c
+        print(history)
     except ValueError:
         c_out.set("Please input a number")
 
 
 def f_convert_func():  # converts the input to a celsius output
-    temp = celsius_input.get()
+    temp_c = celsius_input.get()
     try:
-        temp = float(temp)
-        temp = (temp * (9/5)) + 32
-        temp = round(temp, 2)
-        if temp < -459.67:  # absolute zero in F
-            temp = str(temp) + "°F"
-            f_out.set("Invalid input, temperature is\nbelow absolute zero. ({} < 459.67°F)".format(temp))
-            return
-        temp = str(temp) + "°F"
-        f_out.set(temp)
+        temp_c = float(temp_c)
+        temp_f = (temp_c * (9/5)) + 32
+        temp_f = round(temp_f, 2)
+        if temp_f < -459.67:  # absolute zero in F
+            temp_f = str(temp_f) + "°F"
+            f_out.set("Invalid input, temperature is\nbelow absolute zero. ({} < 459.67°F)".format(temp_f))
+            return  # ends the subroutine
+        temp_f = str(temp_f) + "°F"
+        f_out.set(temp_f)
+        temp_c = str(temp_c) + "°C"
+        history[temp_c] = temp_f
+        print(history)
     except ValueError:
         f_out.set("Please input a number")
+
+
+def help_window_func():
+    help_window = Toplevel(root)
+    help_window.title("Help")
+    help_label = ttk.Label(help_window, text="Temp Converter Help\n Input a number in either box and press the corresponding 'Convert' button\n ")
+    help_label.grid()
+
 
 
 root = Tk()
@@ -52,7 +67,7 @@ exit_button.grid(row=0, column=4, padx=10, pady=10)
 export_button = ttk.Button(main_frame, text="Export as .csv")
 export_button.grid(row=1, column=4, padx=10, pady=10)
 
-help_button = ttk.Button(main_frame, text="Help")
+help_button = ttk.Button(main_frame, text="Help", command=help_window_func)
 help_button.grid(row=4, column=4, padx=10, pady=10)
 
 conversion_history = ttk.Button(main_frame, text="History")
