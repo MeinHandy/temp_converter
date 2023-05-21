0ba390985b9cfa67b048b5343958a7fb3842f7cc
ah19017 <ah19017@melville-high.school.nz>
2023-05-22 09:18:46 +1200

    hello help button you are the best (completed help button)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+import os
+
 history = {}
 
 
@@ -7,7 +9,7 @@ def c_convert_func():  # converts the input to a fahrenheit output
     temp_f = fahrenheit_input.get()
     try:
         temp_f = float(temp_f)
-        temp_c = (temp_f - 32) * (5/9)
+        temp_c = (temp_f - 32) * (5 / 9)
         temp_c = round(temp_c, 2)
         if temp_c < -273.15:  # absolute zero in C
             temp_c = str(temp_c) + "°C"
@@ -26,7 +28,7 @@ def f_convert_func():  # converts the input to a celsius output
     temp_c = celsius_input.get()
     try:
         temp_c = float(temp_c)
-        temp_f = (temp_c * (9/5)) + 32
+        temp_f = (temp_c * (9 / 5)) + 32
         temp_f = round(temp_f, 2)
         if temp_f < -459.67:  # absolute zero in F
             temp_f = str(temp_f) + "°F"
@@ -42,11 +44,29 @@ def f_convert_func():  # converts the input to a celsius output
 
 
 def help_window_func():
+
+    def close_help():
+        help_window.destroy()
+
     help_window = Toplevel(root)
     help_window.title("Help")
-    help_label = ttk.Label(help_window, text="Temp Converter Help\n Input a number in either box and press the corresponding 'Convert' button\n ")
+    help_frame = ttk.LabelFrame(help_window)
+    help_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+    help_label = ttk.Label(help_frame, text="Temp Converter Help\n"  # all the text for the help menu
+                                            "Input a number in either box and press the corresponding 'Convert' button\n"
+                                            "to convert temperatures.\n\n"
+                                            "Press 'export as .csv' to export the temperatures recorded.\n"
+                                            "This file will export to the local directory at: \n"
+                                            "{0}\n\n"
+                                            "Press 'History' to view the program's conversion history. This is the\n"
+                                            "exported data\n\n"
+                                            "Pressing the 'Help' button brings up this menu.\n\n"
+                                            "Pressing the 'Exit' button will close the program without saving.\n"
+                                            "All non-exported data will be lost.".format(os.path.dirname(os.path.realpath(__file__)))
+                           )
     help_label.grid()
-
+    close_help_button = ttk.Button(help_window, text="Close", command=close_help)
+    close_help_button.grid(row=1, column=0, padx=10, pady=10)
 
 
 root = Tk()
@@ -88,6 +108,5 @@ c_out = DoubleVar()
 c_out.set("celsius_output")
 celsius_output = ttk.Label(main_frame, textvariable=c_out)
 celsius_output.grid(row=2, column=1)
-
 
 root.mainloop()
