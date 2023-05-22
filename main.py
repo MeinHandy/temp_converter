df1854dd0c245a8d6230f63f2d715ba8a615de3a
ah19017 <ah19017@melville-high.school.nz>
2023-05-23 10:39:31 +1200

    history is complete and fully functional

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def c_convert_func():  # converts the input to a fahrenheit output
         temp_c = str(temp_c) + "°C"
         c_out.set(temp_c)
         temp_f = str(temp_f) + "°F"
-        history[temp_f] = temp_c
+        history[temp_f] = temp_c  # sets a new input temperature in the dictionary with the value of the output
         print(history)
     except ValueError:
         c_out.set("Please input a number")
@@ -37,8 +37,7 @@ def f_convert_func():  # converts the input to a celsius output
         temp_f = str(temp_f) + "°F"
         f_out.set(temp_f)
         temp_c = str(temp_c) + "°C"
-        history[temp_c] = temp_f
-        print(history)
+        history[temp_c] = temp_f  # sets a new input temperature in the dictionary with the value of the output
     except ValueError:
         f_out.set("Please input a number")
 
@@ -65,8 +64,42 @@ def help_window_func():
                                             "All non-exported data will be lost.".format(os.path.dirname(os.path.realpath(__file__)))
                            )
     help_label.grid()
-    close_help_button = ttk.Button(help_window, text="Close", command=close_help)
+    close_help_button = ttk.Button(help_window, text="Dismiss", command=close_help)
     close_help_button.grid(row=1, column=0, padx=10, pady=10)
+
+
+def history_box_func():
+
+    def close_history():
+        history_window.destroy()
+
+    history_window = Toplevel(root)
+    history_window.title("History")
+    history_frame = ttk.LabelFrame(history_window)
+    history_frame.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+
+    input_label = ttk.Label(history_frame, text="Input")
+    input_label.grid(row=0, column=0)
+
+    output_label = ttk.Label(history_frame, text="Output")
+    output_label.grid(row=0, column=1)
+
+    history_input = Listbox(history_frame)
+    history_input.grid(row=1, column=0)
+    history_input.insert(0, *list(history.keys()))
+
+    history_output = Listbox(history_frame)
+    history_output.grid(row=1, column=1)
+    history_output.insert(0, *list(history.values()))
+
+    close_history = ttk.Button(history_window, text="Dismiss", command=close_history)
+    close_history.grid(row=2, column=0)
+
+
+
+
+def exit_program():
+    exit("Closed program.")
 
 
 root = Tk()
@@ -81,7 +114,7 @@ f_convert_button.grid(row=1, column=0, padx=10, pady=10)
 c_convert_button = ttk.Button(main_frame, text="Convert to celsius", command=c_convert_func)
 c_convert_button.grid(row=1, column=1, padx=10, pady=10)
 
-exit_button = ttk.Button(main_frame, text="Exit")
+exit_button = ttk.Button(main_frame, text="Exit", command=exit_program)
 exit_button.grid(row=0, column=4, padx=10, pady=10)
 
 export_button = ttk.Button(main_frame, text="Export as .csv")
@@ -90,7 +123,7 @@ export_button.grid(row=1, column=4, padx=10, pady=10)
 help_button = ttk.Button(main_frame, text="Help", command=help_window_func)
 help_button.grid(row=4, column=4, padx=10, pady=10)
 
-conversion_history = ttk.Button(main_frame, text="History")
+conversion_history = ttk.Button(main_frame, text="History",command=history_box_func)
 conversion_history.grid(row=2, column=4, padx=10, pady=10)
 
 celsius_input = ttk.Entry(main_frame)
@@ -108,5 +141,6 @@ c_out = DoubleVar()
 c_out.set("celsius_output")
 celsius_output = ttk.Label(main_frame, textvariable=c_out)
 celsius_output.grid(row=2, column=1)
+
 
 root.mainloop()
